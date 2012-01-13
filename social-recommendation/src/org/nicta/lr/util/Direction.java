538a2c4fdb0e82efed10cf75217bfde9928af483
ssanner <ssanner@63c62c83-405f-8d15-9062-c0fcbd917c60>
2012-01-13 19:14:20 +0000

    More data analysis

---
+++ social-recommendation/src/org/nicta/lr/util/Direction.java
@@ -0,0 +1,5 @@
+package org.nicta.lr.util;
+
+public enum Direction {
+	INCOMING, OUTGOING, BIDIR //, COMPLETE
+}
--- social-recommendation/src/org/nicta/lr/util/Interaction.java
+++ social-recommendation/src/org/nicta/lr/util/Interaction.java
@@ -14,21 +14,26 @@ public class Interaction {
 	 * 
 	 * @param uid1
 	 * @param uid2
-	 */
-	public void addInteraction(long uid1, long uid2) {
-		Set<Long> interactions1 = _interactions.get(uid1);
-		if (interactions1 == null) {
-			interactions1 = new HashSet<Long>();
-			_interactions.put(uid1, interactions1);
-		}
-		interactions1.add(uid2);
+	 */		
+	public void addInteraction(long uid1, long uid2, Direction dir) {
 		
-		Set<Long> interactions2 = _interactions.get(uid2);
-		if (interactions2 == null) {
-			interactions2 = new HashSet<Long>();
-			_interactions.put(uid2, interactions2);
+		if (dir == Direction.INCOMING || dir == Direction.BIDIR) {
+			Set<Long> interactions1 = _interactions.get(uid1);
+			if (interactions1 == null) {
+				interactions1 = new HashSet<Long>();
+				_interactions.put(uid1, interactions1);
+			}
+			interactions1.add(uid2);
 		}
-		interactions2.add(uid1);
+		
+		if (dir == Direction.OUTGOING || dir == Direction.BIDIR) {
+			Set<Long> interactions2 = _interactions.get(uid2);
+			if (interactions2 == null) {
+				interactions2 = new HashSet<Long>();
+				_interactions.put(uid2, interactions2);
+			}
+			interactions2.add(uid1);
+		}
 	}
 	
 	public Set<Long> getInteractions(Long uid) {
@@ -44,9 +49,12 @@ public class Interaction {
 	 */
 	public static void main(String[] args) {
 		Interaction i = new Interaction();
-		i.addInteraction(1, 2);
-		i.addInteraction(3, 4);
-		i.addInteraction(1, 3);
+		//Direction dir = Direction.BIDIRECTIONAL;
+		//Direction dir = Direction.INCOMING;
+		Direction dir = Direction.OUTGOING;
+		i.addInteraction(1, 2, dir);
+		i.addInteraction(3, 4, dir);
+		i.addInteraction(1, 3, dir);
 		System.out.println("1: " + i.getInteractions(1l));
 		System.out.println("2: " + i.getInteractions(2l));
 		System.out.println("3: " + i.getInteractions(3l));
