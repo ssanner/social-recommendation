2dd6537340949161ffb80d188232fd0687b0ffad
ssanner <ssanner@63c62c83-405f-8d15-9062-c0fcbd917c60>
2012-01-17 13:48:16 +0000

    More data

--- social-recommendation/doc/icwsm/data/get_index.m
+++ social-recommendation/doc/icwsm/data/get_index.m
@@ -1,3 +1,3 @@
 function [index] = get_index(avg, dir, ltype, itype)
 
-index = (avg - 1).*315 + (dir - 1).*105 + (ltype - 1).*21 + itype;
+index = (avg - 1).*330 + (dir - 1).*110 + (ltype - 1).*22 + itype;
