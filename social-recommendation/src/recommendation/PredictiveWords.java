b7a9a7c2b07bc15d1753b30a05812a620075ec55
rileyjkidd <rileyjkidd@gmail.com>
2012-03-04 23:24:49 +0000

    Setting up recommendation package

--- social-recommendation/src/recommendation/PredictiveWords.java
+++ social-recommendation/src/recommendation/PredictiveWords.java
@@ -1,9 +1,16 @@
 package recommendation;
 
+import java.sql.SQLException;
+
+import org.nicta.lr.util.UserUtil;
+
 public class PredictiveWords {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		System.out.println("test");
+		for (Long s : UserUtil.getAppUserIds()){
+			System.out.println(s);
+		}
 	}
 	
 }
