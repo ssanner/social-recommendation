0765ec1e580d688cd707c82cb0ebde0c111d998b
rileyjkidd@gmail.com <rileyjkidd@gmail.com@63c62c83-405f-8d15-9062-c0fcbd917c60>
2012-03-05 01:13:53 +0000

    Setting up recommendation package

--- trunk/social-recommendation/src/recommendation/PredictiveWords.java
+++ trunk/social-recommendation/src/recommendation/PredictiveWords.java
@@ -3,31 +3,49 @@ package recommendation;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Map;
 import java.util.Set;
 
 import org.nicta.lr.util.EDirectionType;
 import org.nicta.lr.util.EInteractionType;
 import org.nicta.lr.util.SQLUtil;
+import org.nicta.lr.util.UserUtil;
 
 public class PredictiveWords {
 
+	public static Set<Long> APP_USERS;
+	public static Set<Long> ALL_USERS;
+	public static Map<Long,String> UID_2_NAME;
+	
+	static {
+		try {
+		APP_USERS = UserUtil.getAppUserIds();
+		ALL_USERS = UserUtil.getUserIds();
+		UID_2_NAME = UserUtil.getUserNames();
+		} catch (SQLException e) {
+			System.out.println(e);
+			System.exit(1);
+		}
+	}
+	
+	
 	public static void main(String[] args) throws SQLException {				
 		PredictiveWords p = new PredictiveWords();
 		p.getAllComments(EInteractionType.ALL_COMMENTS, EDirectionType.OUTGOING);
 	}
 
 
-	public static CommentsInteraction getUserComments(EInteractionType type, EDirectionType dir) 
+	public static Interaction getUserComments(EInteractionType type, EDirectionType dir) 
 	throws SQLException
 {
-	CommentsInteraction i = new CommentsInteraction(); // currently treat interactions as undirected
+	Interaction i = new Interaction(); // currently treat interactions as undirected
 
 	// Repeated calls or direct
 	if (type == EInteractionType.ALL_COMMENTS) {
-		CommentsInteraction link_comments  = getUserComments(EInteractionType.LINK_COMMENTS, dir);
-		CommentsInteraction post_comments  = getUserComments(EInteractionType.POST_COMMENTS, dir);
-		CommentsInteraction photo_comments = getUserComments(EInteractionType.PHOTO_COMMENTS, dir);
-		CommentsInteraction video_comments = getUserComments(EInteractionType.VIDEO_COMMENTS, dir);
+		Interaction link_comments  = getUserComments(EInteractionType.LINK_COMMENTS, dir);
+		Interaction post_comments  = getUserComments(EInteractionType.POST_COMMENTS, dir);
+		Interaction photo_comments = getUserComments(EInteractionType.PHOTO_COMMENTS, dir);
+		Interaction video_comments = getUserComments(EInteractionType.VIDEO_COMMENTS, dir);
 		link_comments.addAllInteractions(post_comments);
 		link_comments.addAllInteractions(photo_comments);
 		link_comments.addAllInteractions(video_comments);
@@ -68,16 +86,16 @@ public class PredictiveWords {
 	
 	
 	public void getAllComments(EInteractionType type, EDirectionType dir) throws SQLException{		
-		CommentsInteraction i = getUserComments(EInteractionType.ALL_COMMENTS, EDirectionType.OUTGOING);
-		for (long uid : ExtractRelTables.APP_USERS) {
-			String uid_name = ExtractRelTables.UID_2_NAME.get(uid);				
+		Interaction i = getUserComments(EInteractionType.ALL_COMMENTS, EDirectionType.OUTGOING);
+		for (long uid : APP_USERS) {
+			String uid_name = UID_2_NAME.get(uid);				
 			Set<Long> inter = i.getInteractions(uid);
 			System.out.println(uid + ", " + uid_name + " -- " + type + ": " + (inter == null ? 0 : inter.size()));
 			System.out.print(" * [ ");
 			boolean first = true;
 			if (inter != null) 
 				for (Long uid2 : inter) {
-					String uid2_name = ExtractRelTables.UID_2_NAME.get(uid2);
+					String uid2_name = UID_2_NAME.get(uid2);
 					System.out.print((first ? "" : ", ") + uid2_name);
 					first = false;
 				}
