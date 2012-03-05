0b4567396b1dc9c7e2a04993bbb1e367635ec9e4
rileyjkidd <rileyjkidd@gmail.com>
2012-03-05 00:29:36 +0000

    Setting up recommendation package

--- social-recommendation/src/recommendation/PredictiveWords.java
+++ social-recommendation/src/recommendation/PredictiveWords.java
@@ -18,7 +18,7 @@ public class PredictiveWords {
 		
 		Statement statement = SQLUtil.getStatement();
 		
-		String userQuery = "SELECT uid FROM linkrlikes";
+		String userQuery = "SELECT uid FROM linkrlinkcomments";
 		
 		ResultSet result = statement.executeQuery(userQuery);
 		while (result.next()) {
@@ -31,6 +31,56 @@ public class PredictiveWords {
 	}
 
 
+	public static Interaction getUserInteractions(EInteractionType type, EDirectionType dir) 
+	throws SQLException
+{
+	Interaction i = new Interaction(); // currently treat interactions as undirected
+
+	// Repeated calls or direct
+	if (type == EInteractionType.ALL_COMMENTS) {
+		Interaction link_comments  = getUserInteractions(EInteractionType.LINK_COMMENTS, dir);
+		Interaction post_comments  = getUserInteractions(EInteractionType.POST_COMMENTS, dir);
+		Interaction photo_comments = getUserInteractions(EInteractionType.PHOTO_COMMENTS, dir);
+		Interaction video_comments = getUserInteractions(EInteractionType.VIDEO_COMMENTS, dir);
+		link_comments.addAllInteractions(post_comments);
+		link_comments.addAllInteractions(photo_comments);
+		link_comments.addAllInteractions(video_comments);
+		return link_comments;		
+	} else { 
+	
+		// Base case retrieval
+		String table = null;
+		String target_uid = null;
+		String interacting_uid = null;
+		switch (type) {
+			case LINK_COMMENTS:  table = "linkrlinkcomments"; target_uid = "uid"; interacting_uid = "from_id"; break;
+			case POST_COMMENTS:  table = "linkrpostcomments"; target_uid = "uid"; interacting_uid = "from_id"; break;
+			case PHOTO_COMMENTS: table = "linkrphotocomments"; target_uid = "uid"; interacting_uid = "from_id"; break; 			
+			case VIDEO_COMMENTS: table = "linkrvideocomments"; target_uid = "uid"; interacting_uid = "from_id"; break;
+			default: {
+				System.out.println("ERROR: Illegal type -- " + type);
+				System.exit(1);
+			}
+		}
+		
+		String sql_query = "SELECT " + target_uid + ", " + interacting_uid + ", message FROM " + table;
+		
+		Statement statement = SQLUtil.getStatement();
+		ResultSet result = statement.executeQuery(sql_query);
+		while (result.next()) {
+			// INCOMING if in correct order
+			//long TARGET_ID = result.getLong(1);
+			//long FROM_ID = result.getLong(2);
+			//i.addInteraction(TARGET_ID, FROM_ID, type == EInteractionType.FRIENDS ? EDirectionType.BIDIR : dir);
+			System.out.println(result);
+		}
+		statement.close();
+		
+		return i;
+	}
+}
+	
+	
 	public void getAllComments(EInteractionType type, EDirectionType dir) throws SQLException{		
 		Interaction i = UserUtil.getUserInteractions(EInteractionType.ALL_COMMENTS, EDirectionType.OUTGOING);
 		for (long uid : ExtractRelTables.APP_USERS) {
