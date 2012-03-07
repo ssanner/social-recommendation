598393a7a44f632f8fd7b56ac02a0830852527e3
rileyjkidd <rileyjkidd@gmail.com>
2012-03-07 03:47:41 +0000

    Message frequency source

--- social-recommendation/src/recommendation/PredictiveWords.java
+++ social-recommendation/src/recommendation/PredictiveWords.java
@@ -43,7 +43,27 @@ public class PredictiveWords {
 	public static void main(String[] args) throws SQLException, IOException, LangDetectException {				
 		PredictiveWords p = new PredictiveWords();
 		//p.writeUserComments();
-		p.buildCommentsDictionary(MESSAGES_FILE);
+		//p.buildCommentsDictionary(MESSAGES_FILE);
+		
+		
+		
+		
+		
+		
+		DetectorFactory.loadProfile(Configuration.LANG_PROFILE_FOLDER);
+		Detector messageDetector = DetectorFactory.create();
+		messageDetector.append("february");				
+		String messageLang = messageDetector.detect();
+		
+		System.out.println(messageLang);
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 	
 	public void buildCommentsDictionary(String fileName) throws IOException, LangDetectException{
