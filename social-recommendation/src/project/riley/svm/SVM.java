b60d0ec4e854139c08b25cd9cad203acaa6c6041
rileyjkidd <rileyjkidd@gmail.com>
2012-04-23 04:24:35 +0000

    SVM package

--- social-recommendation/src/project/riley/svm/SVM.java
+++ social-recommendation/src/project/riley/svm/SVM.java
@@ -1,5 +1,56 @@
 package project.riley.svm;
 
-public class SVM {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class SVM {
+	
+	/*
+	 * convert from arff to libsvm format
+	 * class <index>:<value>
+	 */
+	public void convertArff(String file) throws IOException{
+		BufferedReader in = new BufferedReader(new FileReader(file));
+		BufferedWriter out = new BufferedWriter(new FileWriter(file.split(".")[0] + ".libsvm"));
+		String line;
+		while((line = in.readLine()) != null){
+			if (!line.startsWith("@") && line.length() > 0){
+				writeArffLine(line, out);
+			}											
+		}			
+		out.close();
+	}
+	
+	/*
+	 * write a line using the arff data
+	 */
+	public void writeArffLine(String line, BufferedWriter out) throws IOException{
+		String[] parts = line.split(",");
+
+		// third item is class
+		out.write(yTo1nTo0(parts[3]));
+		
+		// first two parts of line are item id and user id
+		for (int i = 3; i < parts.length; i++){
+			out.write(" " + (i-2) + ":" + yTo1nTo0(parts[3]));
+		}
+		out.write("\n");
+	}
+	
+	/*
+	 * input is either 'n' or 'y'
+	 */
+	public int yTo1nTo0(String str){
+		return (str.charAt(1) == 'n' ? 0 : 1);
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		SVM svm = new SVM();
+		svm.convertArff("datak100.arff");
+	}
+	
 }
