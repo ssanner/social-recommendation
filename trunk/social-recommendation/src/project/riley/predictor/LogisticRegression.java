b1f423079a012563faa40abbe470a354f091d9ea
rileyjkidd@gmail.com <rileyjkidd@gmail.com@63c62c83-405f-8d15-9062-c0fcbd917c60>
2012-04-26 02:56:20 +0000

    Logistic Regression Class

--- trunk/social-recommendation/src/project/riley/predictor/LogisticRegression.java
+++ trunk/social-recommendation/src/project/riley/predictor/LogisticRegression.java
@@ -108,7 +108,10 @@ public class LogisticRegression extends Predictor {
 		LogisticRegression lr = new LogisticRegression();
 	//	lr.runTests();
 		System.out.println(lr.data._data.get(0));
-		System.out.println(lr.getFeatures(lr.data._data.get(0), lr.data._attr.size()-2));
+		double[] a =lr.getFeatures(lr.data._data.get(0), lr.data._attr.size()-2);
+		for (double b : a){
+			System.out.print(b + " ");
+		}
 	}
 	
 }
