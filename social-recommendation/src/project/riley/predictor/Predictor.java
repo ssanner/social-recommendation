7f6a34438359e7093fe029f29097946f10006e50
rileyjkidd <rileyjkidd@gmail.com>
2012-05-28 04:58:19 +0000

    ROC updates

--- social-recommendation/src/project/riley/predictor/Predictor.java
+++ social-recommendation/src/project/riley/predictor/Predictor.java
@@ -13,7 +13,8 @@ public abstract class Predictor {
 	String dataFile = "datak1000.arff";
 	ArffData data = new ArffData(dataFile);
 	DecimalFormat df3 = new DecimalFormat("#.###");
-	private double[] _thresholds = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9};
+	//private double[] _thresholds = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9};
+	private double[] _thresholds = {0.5};
 	private int _iterations = 10;
 	public int _classIndex = 2;
 	
