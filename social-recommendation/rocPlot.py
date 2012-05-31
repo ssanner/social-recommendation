4cee3d7320560f2b6aa8b38ae9471dd19bc8d7e4
rileyjkidd <rileyjkidd@gmail.com>
2012-05-31 03:58:41 +0000

    Dummy data

--- social-recommendation/rocPlot.py
+++ social-recommendation/rocPlot.py
@@ -10,9 +10,9 @@ for file, name, type in plots:
   plt.plot([row[2] for row in data],[row[1] for row in data], type, marker='o', label=name)  
   for label, x, y in [(row[0] , row[2], row[1]) for row in data]:
    plt.annotate(
-       label, xy = (x, y), xytext = (7, 7), textcoords = 'offset points', ha = 'right', va = 'bottom',
+       label, xy = (x, y), xytext = (0, 12), textcoords = 'offset points', ha = 'center', va = 'center',
        bbox = dict(boxstyle = 'round,pad=0.2', fc = 'yellow', alpha = 0.5)) 
- 
+  
 plt.title("ROC Plot for 0.1 < Threshold < 0.9")
 plt.xlabel("False positive rate")
 plt.ylabel("Correct class identification rate")
