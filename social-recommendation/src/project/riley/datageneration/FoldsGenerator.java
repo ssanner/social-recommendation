d202796bf55e73ac6c9a6307bb3f92dd66bfaf8a
rileyjkidd <rileyjkidd@gmail.com>
2012-06-09 07:52:36 +0000

    Folds update

--- social-recommendation/src/project/riley/datageneration/FoldsGenerator.java
+++ social-recommendation/src/project/riley/datageneration/FoldsGenerator.java
@@ -2,6 +2,8 @@ package project.riley.datageneration;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
 
 import project.riley.predictor.ArffData;
 import project.riley.predictor.ArffData.DataEntry;
@@ -11,19 +13,53 @@ import project.riley.predictor.ArffData.DataEntry;
  * 
  */
 public class FoldsGenerator {
-	int totalFolds = 10;
-	String fileName = "a.arff";
-	PrintWriter writer;
-	
-	String[] directions = new String[]{"Incoming", "Outgoing"};					
-	String[] interactionMedium = new String[]{"Post", "Photo", "Video", "Link"};
-	String[] interactionType = new String[]{"Comments", "Tags", "Likes"};
-	
+	int totalFolds = 10;			// total folds to generate
+	String fileName = "a.arff";		// arff file to read
+	String header = "al"; 			// al || pl
+
 	/*
-	 * write header data for each fold
+	 * read file to split into validation sets
 	 */
-	public void writeHeader(PrintWriter writer, String fileName) throws FileNotFoundException{
-		writer = new PrintWriter(fileName);		
+	public ArffData readArff(){
+		return new ArffData(fileName);
+	}
+
+	/*
+	 * write validation sets
+	 * 
+	 */
+	public void writeFolds(ArffData data) throws FileNotFoundException{
+		int size = data._data.size();				// size of the data set
+		int start = 0;								// start index
+		int testFold = 0;							// fold to test (others are train)
+		int stepSize = size/totalFolds;				// step size
+		int extra = size % totalFolds;
+		for (int i = 0; i < size; i += stepSize){
+			if (extra > 0){
+				i++;
+				extra--;
+			}
+			//System.out.println(start + ":" + i + ":" + (i+stepSize) + ":" + size);
+			List<DataEntry> testSet = data._data.subList(i, i+stepSize);			// training indices
+			List<DataEntry> trainSet = data._data.subList(start, i);				// testing indices from start to train start
+			trainSet.addAll(data._data.subList(i+stepSize, size));					// testing indices from train end to list end
+			testFold++;
+
+			writeFoldData(header + "_test_fold" + testFold + ".arff",testSet);
+			writeFoldData(header + "_train_fold" + testFold + ".arff",trainSet);
+		}
+	}
+
+	/*
+	 * write fold data including header data for each fold
+	 * {pl||al}_train_fold{0..9}.arff 
+	 */
+	public void writeFoldData(String fileName, List<DataEntry> data) throws FileNotFoundException{
+		String[] directions = new String[]{"Incoming", "Outgoing"};					
+		String[] interactionMedium = new String[]{"Post", "Photo", "Video", "Link"};
+		String[] interactionType = new String[]{"Comments", "Tags", "Likes"};
+
+		PrintWriter writer = new PrintWriter(fileName);		
 		writer.println("@relation app-data");
 		writer.println("@attribute 'Uid' numeric");
 		writer.println("@attribute 'Item' numeric");
@@ -39,39 +75,17 @@ public class FoldsGenerator {
 			}
 		}
 		writer.println("@data");
-	}
-	
-	/*
-	 * read file to split into validation sets
-	 */
-	public ArffData readArff(){
-		return new ArffData(fileName);
-	}
-	
-	/*
-	 * write validation sets
-	 * 
-	 */
-	public void writeFolds(ArffData data){
-		int size = data._data.size();				// size of the data set
-		int testFold = 0;							// fold to test
-		int stepSize = size/totalFolds;				// step size
-		int extraSteps = size % totalFolds;			// left over parts
-		for (int i = 0; i < size; i += stepSize){
-			System.out.println(i);
-			testFold++;
-			/*for (DataEntry de : data._data){
-				for (int i = 0; i < data._attr.size(); i++){
-					System.out.print(de.getData(i) + " ");
-				}
-			}*/
+		for (DataEntry de : data) {   
+			//writer.println(de);
+			System.out.println(de);
 		}
+		writer.close();
 	}
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws FileNotFoundException {
 		FoldsGenerator fg = new FoldsGenerator();
 		ArffData data = fg.readArff();
 		fg.writeFolds(data);
 	}
-	
+
 }
