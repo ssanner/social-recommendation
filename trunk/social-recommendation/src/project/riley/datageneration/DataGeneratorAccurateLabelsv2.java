9c120975bb1379b163f2ba7e4c839659dc64765e
ssanner <ssanner@63c62c83-405f-8d15-9062-c0fcbd917c60>
2012-06-15 13:10:30 +0000

    Updated data generator

--- trunk/social-recommendation/src/project/riley/datageneration/DataGeneratorAccurateLabelsv2.java
+++ trunk/social-recommendation/src/project/riley/datageneration/DataGeneratorAccurateLabelsv2.java
@@ -11,152 +11,194 @@ import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.Map.Entry;
 
-import org.nicta.lr.util.SQLUtil;
+import org.nicta.lr.util.*;
 
 public class DataGeneratorAccurateLabelsv2 {
 
-	public final static String YES = "y".intern();
-	public final static String NO  = "n".intern();
+	public static final String YES = "'y'".intern();
+	public static final String NO  = "'n'".intern();
+	public static final String[] RATINGS = new String[]{ YES, NO };
+	
+	/*
+	 * For caching data
+	 */
+	public static ArrayList<EInteractionType> _featuresInt = null;
+	public static ArrayList<EDirectionType>   _featuresDir = null;
+	public static Map<Long,Set<Long>> _uid2all_passive_linkids_likes = null;
+	public static Map<Long,Set<Long>> _uid2linkids_likes = null;
+	public static Map<Long,Set<Long>> _uid2linkids_dislikes = null;
+	public static Map<EInteractionType,Map<EDirectionType,Map<Long,Set<Long>>>> _int_dir2uid_linkid = null;
 	
 	/*
 	 * Generate data for accurately labeled data from NICTA app
 	 */
-	static PrintWriter writer;
-	static String[] directions = new String[]{"Incoming", "Outgoing"};					
-	static String[] interactionMedium = new String[]{"Post", "Photo", "Video", "Link"};
-	static String[] interactionType = new String[]{"Comments", "Tags", "Likes"};
-	static String[] likesRow = new String[]{"post_id", "photo_id", "video_id", "link_id"};
-	static String[] likesTable = new String[]{"linkrPostLikes", "linkrPhotoLikes", "linkrVideoLikes", "linkrLinkLikes"};
+	public static PrintWriter _writer;
+	
+	public static void populateCachedData(boolean active_likes) throws SQLException {
+		
+		////////////////////////////////////////////////////////////////////////
+		// Get dataset of likes / dislikes for active or passive
+		////////////////////////////////////////////////////////////////////////
+		_uid2all_passive_linkids_likes = UserUtil.getLikes(ELikeType.LINK);
+		if (active_likes) {
+			_uid2linkids_likes = new HashMap<Long,Set<Long>>();
+			_uid2linkids_dislikes = new HashMap<Long,Set<Long>>();
+			
+			String userQuery = "select uid, link_id, rating from trackRecommendedLinks where rating != 0;"; // 0 = not rated
+			Statement statement = SQLUtil.getStatement();
+
+			ResultSet result = statement.executeQuery(userQuery);
+			while (result.next()) {
+				long uid = result.getLong(1);
+				long link_id = result.getLong(2);
+				// 0 = not rated, 1 = liked, 2 = not liked
+				int rating = result.getInt(3);
+				
+				// SPS - cannot do this yet... 0 should be thrown out first
+				// userLikes.put(link_id, (rating == 1 ? "y" : "n"));
+				if (rating < 1 || rating > 2)
+					continue;
+				boolean is_liked = (rating == 1);
+				
+				Set<Long> userLikes = 
+					(is_liked ? _uid2linkids_likes : _uid2linkids_dislikes).get(uid);
+				if (userLikes == null){
+					userLikes = new HashSet<Long>();
+					(is_liked ? _uid2linkids_likes : _uid2linkids_dislikes).put(uid, userLikes);
+				}
+				userLikes.add(link_id);
+			}
+			
+		} else {
+			System.out.println("Cannot handle passive likes yet");
+			System.exit(1);
+			_uid2linkids_likes = _uid2all_passive_linkids_likes;
+			// passive Facebook data
+			// rank top k, filter and infer dislikes
+		}
+		
+		////////////////////////////////////////////////////////////////////////
+		// For every user and interaction type/dir, get set of items liked by 
+		//   that set of alters (threshold for now at 1)
+		////////////////////////////////////////////////////////////////////////
+		_featuresInt = new ArrayList<EInteractionType>();
+		_featuresDir = new ArrayList<EDirectionType>();
+		_int_dir2uid_linkid = new HashMap<EInteractionType,Map<EDirectionType,Map<Long,Set<Long>>>>();
+		for (EInteractionType itype : EInteractionType.values()) {
+			
+			// Don't need interactions beyond real
+			if (itype == EInteractionType.REAL)
+				break;
+			
+			Map<EDirectionType,Map<Long,Set<Long>>> dir2rest = _int_dir2uid_linkid.get(itype);
+			if (dir2rest == null) {
+				dir2rest = new HashMap<EDirectionType,Map<Long,Set<Long>>>();
+				_int_dir2uid_linkid.put(itype, dir2rest);
+			}
+				
+			for (EDirectionType dir : EDirectionType.values()) {
+	
+				if (dir == EDirectionType.BIDIR)
+					continue;
+				else if (itype == EInteractionType.FRIENDS && dir == EDirectionType.OUTGOING)
+					continue;
+				
+				// Maintain feature list in parallel arrays of int/dir
+				_featuresInt.add(itype);
+				_featuresDir.add(dir);
+				
+				Map<Long,Set<Long>> uid2rest = dir2rest.get(dir);
+				if (uid2rest == null) {
+					uid2rest = new HashMap<Long,Set<Long>>();
+					dir2rest.put(dir, uid2rest);
+				}
+
+				Interaction i = UserUtil.getUserInteractions(itype, dir);
+	
+				System.out.println("**********************\n" + _featuresInt + "\n" + _featuresDir);
+				System.out.println("**********************\n" + itype + "_" + dir + " #alters = " + i.getAllInteractions().size());
+
+				for (Long uid : _uid2linkids_likes.keySet()) {
+										
+					// ** Note: these are passive likes on Facebook!
+					Set<Long> alters = i.getInteractions(uid);
+					System.out.println(ExtractRelTables.UID_2_NAME.get(uid) + " " + itype + "_" + dir + " #alters = " + (alters == null ? 0 : alters.size()));
+
+					HashMap<Long,Integer> other_likes_id2count = ExtractRelTables.GetLikesInteractions(uid, i, _uid2all_passive_linkids_likes);
+					//P(like | friend likes) = P(like and friend likes) / P(friend likes)
+					//                       = F(like and friend likes) / F(friend likes)
+					Set<Long> other_likes_ids = ExtractRelTables.ThresholdAtK(other_likes_id2count, /*k*/1);
+					if (other_likes_ids == null)
+						other_likes_ids = new HashSet<Long>();
+					uid2rest.put(uid, other_likes_ids);
+					
+					System.out.println("-- count of set of links liked by alters under uid,itype,dir: " + other_likes_ids.size());
+				}
+			}
+		}
+	}
 	
 	/*
 	 * Write arff header data
 	 */
-	public static void writeHeader(String fileName) throws FileNotFoundException{
-		writer = new PrintWriter(fileName);		
-		writer.println("@relation app-data");
-		writer.println("@attribute 'Uid' numeric");
-		writer.println("@attribute 'Item' numeric");
-		writer.println("@attribute 'Class' { 'n' , 'y' }");
-		for (String direction : directions){
-			for (String interaction : interactionMedium){
-				for (int i = 0; i < interactionType.length; i++){
-					if (interaction.equals("Link") && interactionType[i].equals("Tags")){
-						continue; // no link tags data
-					}
-					writer.println("@attribute '" + direction + "-" + interaction + "-" + interactionType[i] + "' { 'n', 'y' }");
-				}
-			}
+	public static void writeHeader(String fileName) throws Exception {
+		_writer = new PrintWriter(fileName);		
+		_writer.println("@relation app-data");
+		_writer.println("@attribute 'Uid' numeric");
+		_writer.println("@attribute 'Item' numeric");
+		_writer.println("@attribute 'Class' { 'n' , 'y' }");
+		for (int feat_index = 0; feat_index < _featuresInt.size(); feat_index++) {
+			_writer.println("@attribute '" + 
+					_featuresDir.get(feat_index) + "_" + 
+					_featuresInt.get(feat_index) + "' { 'n', 'y' }");
 		}
-		writer.println("@data");
-	}
-
-	/*
-	 * Extract data for known ratings
-	 */
-	public static HashMap<Long, HashMap<Long, String>> extractSpecificLikes() throws SQLException{
-		HashMap<Long, HashMap<Long, String>> userLinks = new HashMap<Long, HashMap<Long, String>>();
-		String userQuery = "select uid, link_id, rating from trackRecommendedLinks where rating != 0;"; // 0 = not rated
-		Statement statement = SQLUtil.getStatement();
-
-		ResultSet result = statement.executeQuery(userQuery);
-		while (result.next()) {
-			long uid = result.getLong(1);
-			long link_id = result.getLong(2);
-			// 0 = not rated, 1 = liked, 2 = not liked
-			int rating = result.getInt(3);
 			
-			// SPS - cannot do this yet... 0 should be thrown out first
-			// userLikes.put(link_id, (rating == 1 ? "y" : "n"));
-			if (rating < 1 || rating > 2)
-				continue;
-			
-			HashMap<Long, String> userLikes = userLinks.get(uid);
-			if (userLikes == null){
-				userLikes = new HashMap<Long, String>();
-				userLinks.put(uid, userLikes);
-			}
-			
-			// SPS - use constants... prevents creating 100,000 Strings
-			userLikes.put(link_id, (rating == 1 ? YES : NO));
-			
-			// SPS - why doing this here?  only if creating a new one
-			//userLinks.put(uid, userLikes);
-		}							
-		return userLinks;
+		_writer.println("@data");
 	}
 
 	/*
 	 * Write known rating data
 	 */
-	public static void writeData(HashMap<Long, HashMap<Long, String>> userLinks) throws SQLException{
+	public static void writeData(String filename) throws Exception {
 		
-		System.out.println("Extracting ratings data for " + userLinks.size() + " users");
-		for (Entry<Long, HashMap<Long, String>> uid : userLinks.entrySet()){
-			Long user = uid.getKey();
-			HashMap<Long, String> userLikes = uid.getValue();
-			System.out.println("User " + user + " made " + userLikes.size() + " ratings");
-			for (Entry<Long, String> likes : userLikes.entrySet()){
-				Long likeId = likes.getKey();
-				String rating = likes.getValue();
-				writer.print(user + "," + likeId + "," + "'" + rating + "'");
-				buildFCols(user, likeId);
-			}
-		}
-	}
+		writeHeader(filename);
+		
+		System.out.println("Extracting ratings data for " + _uid2linkids_likes.size() + " users");
 
-	/*
-	 * build f(i) columns for each (user, like) item pair
-	 * i = {ingoing,outgoing} X {post,photo,video,link} X {comment,tag,like}
-	 * alters(i) = all users who have interacted with (user) via (i)
-	 * column is set to 1 if any of the alters have also liked the item associated with the user otherwise 0
-	 */
-	public static void buildFCols(Long uid, Long lid) throws SQLException{
-		Statement statement = SQLUtil.getStatement();
+		for (String rating : RATINGS) {
 
-		String[] row = new String[]{"from_id", "uid1", "id"};			// tables have different names for in/out cols
-		String[] where = new String[]{"uid", "uid2", "uid"};
-		String getRow;
-		String getWhere;
+			Map<Long,Set<Long>> uid2links = 
+				(rating == YES ? _uid2linkids_likes : _uid2linkids_dislikes); 
 
-		for (String direction : directions){
-			for (String interaction : interactionMedium){
-				for (int i = 0; i < interactionType.length; i++){
-					if (interaction.equals("Link") && interactionType[i].equals("Tags")){
-						continue; // no link tags data
-					} 
-
-					if (direction.equals("Outgoing")){		// outgoing order
-						getRow = row[i];
-						getWhere = where[i];
-					} else {								// incoming order
-						getRow = where[i];
-						getWhere = row[i];
-					}								
-
-					// select incoming/outgoing data for different interaction types
-					String userQuery = "SELECT " + getRow + " FROM linkr" + interaction + interactionType[i] + " WHERE " + getWhere + " = " + uid;
-					boolean found = false;
-
-					ResultSet result = statement.executeQuery(userQuery);
-					while (result.next()) {
-						if (userLikes(result.getLong(1),lid,likesRow[i],likesTable[i])){	// if a user in alter set has liked the original item
-							writer.print(",'y'");
-							found = true;
-							break;
-						}
+			for (Entry<Long, Set<Long>> entry : uid2links.entrySet()){
+				Long uid = entry.getKey();
+				Set<Long> link_ids = entry.getValue();
+				System.out.println("User " + ExtractRelTables.UID_2_NAME.get(uid) + " made " + link_ids.size() + " " + rating + " ratings");
+				for (Long link_id : link_ids){
+					_writer.print(uid + "," + link_id + "," + rating);
+					
+					// Now write columns
+					for (int feat_index = 0; feat_index < _featuresInt.size(); feat_index++) {
+						//System.out.println("Writing feature: " + _featuresDir.get(feat_index) + "_" + _featuresInt.get(feat_index));
+						Set<Long> alter_likes = _int_dir2uid_linkid.
+							get(_featuresInt.get(feat_index)).get(_featuresDir.get(feat_index)).get(uid);
+						String feat_value = alter_likes == null ? NO :
+							(alter_likes.contains(link_id) ? YES : NO);
+						_writer.print("," + feat_value);
 					}
-					if (!found){														// if no user has liked the original item
-						writer.print(",'n'");
-					}
-
+					_writer.println();
 				}
 			}
 		}
-		statement.close();
-		writer.println();
+		
+		_writer.close();
 	}
 
 	/*
@@ -171,11 +213,9 @@ public class DataGeneratorAccurateLabelsv2 {
 	}
 
 
-	public static void main(String[] args) throws FileNotFoundException, SQLException {
-		writeHeader("accurateLabelsData.arff");
-		HashMap<Long, HashMap<Long, String>> userLinks = extractSpecificLikes();
-		writeData(userLinks);
-		writer.close();
+	public static void main(String[] args) throws Exception {
+		populateCachedData(true /* active */);
+		writeData("accurateLabelsData.arff");
 	}
 
 }
