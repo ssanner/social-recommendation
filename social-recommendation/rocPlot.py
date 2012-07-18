f222054c8e24565c6a171f45f27dd6eb633b755e
rileyjkidd <rileyjkidd@gmail.com>
2012-07-18 01:17:30 +0000

    new interaction tests

--- social-recommendation/rocPlot.py
+++ social-recommendation/rocPlot.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-import pylab as plt 
+import pylab as plt
 from pylab import *
 from collections import defaultdict
 
@@ -10,30 +10,24 @@ resultsDictionary = defaultdict(list)
 running = ""
 for line in results:
   if "Running" in ''.join(line):
-    running = ''.join(line).split(" ")[1]						# hashmap key
+    running = ''.join(line).split(" ")[1]											# hashmap key
   if "Accuracy" in ''.join(line):
     resultsDictionary.setdefault(running,[]).append(''.join(line).split(" ")[2])	# accuracy values for key
 
-fig1 = plt.figure(figsize = (10,15))
-plt.subplots_adjust(hspace=1.0)
-
 print resultsDictionary.items()
-
-plotCount = 1
+print "Generating " , len(resultsDictionary) , " graphs"
+ 
+count = 0
 for key in resultsDictionary:
-  count = 1
-  p1 = plt.subplot(10,1,plotCount)
-  plotCount+=1										# feature count
-  for item in resultsDictionary[key]:
-    #print count , item , key
-    plt.plot(item, count, marker='o')
-    plt.ylim((0,10))
-    plt.xlim((0.0,1.0))
-    plt.title("Thresholding for feature size using " + key)
-    plt.xlabel("Accuracy")
-    plt.ylabel("Feature Size")
-    plt.grid(True)
-    count+=1
+	plt.figure(count)
+	plt.plot(range(0,10), resultsDictionary[key],'ro-')
+	plt.ylim((0.0,1.0))
+	plt.xlim((0,10))
+	plt.title("Thresholding for feature size using " + key)
+	plt.xlabel("Feature Size")
+	plt.ylabel("Accuracy")
+	plt.grid(True)
+	count+=1
 
 '''
 for file, name, type in plots:
@@ -44,5 +38,6 @@ for file, name, type in plots:
        label, xy = (x, y), xytext = (0, 12), textcoords = 'offset points', ha = 'center', va = 'center',
        bbox = dict(boxstyle = 'round,pad=0.2', fc = 'yellow', alpha = 0.5)) 
 '''
+
 #plt.savefig('plot.png')
 plt.show();
