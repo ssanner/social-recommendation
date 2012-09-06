271d0870722d0de1feccb7862c76b8f7b418a5c6
rileyjkidd <rileyjkidd@gmail.com>
2012-09-06 02:10:00 +0000

    user info hack update

--- social-recommendation/src/project/riley/datageneration/UserInfoHack.java
+++ social-recommendation/src/project/riley/datageneration/UserInfoHack.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 import org.nicta.lr.util.SQLUtil;
@@ -15,15 +16,21 @@ import project.ifilter.messagefrequency.PredictiveWords;
 
 public class UserInfoHack {
 
+	// top n words to test against
+	public static int TOPN = 5;
+	
+	// store messages
 	static String INCOMING_MESSAGES_FILE = "user_messages_incoming.txt";
 	static String OUTGOING_MESSAGES_FILE = "user_messages_outgoing.txt";
 
+	// store users who used top words
 	static HashSet<Long> INCOMING_SEEN = new HashSet<Long>();
 	static HashSet<Long> OUTGOING_SEEN = new HashSet<Long>();
 	
 	static String INCOMING = "incoming_seen.txt";
 	static String OUTGOING = "outgoing_seen.txt";
 
+	// extract incoming/outgoing messages to corresponding files
 	public static void getMessageInfo() throws Exception{		
 
 		StringBuilder users = new StringBuilder();
@@ -33,7 +40,6 @@ public class UserInfoHack {
 
 		String usersToGet = users.toString().substring(0, users.length()-1);
 		System.out.println("Extracting users info for: (" + usersToGet + ")");
-
 
 		PrintWriter outgoingWriter = new PrintWriter(OUTGOING_MESSAGES_FILE);
 		PrintWriter incomingWriter = new PrintWriter(INCOMING_MESSAGES_FILE);
@@ -45,7 +51,6 @@ public class UserInfoHack {
 		for (String table : tables){
 
 			String sql_query = "SELECT uid, message FROM " + table + " where uid in  (" + usersToGet + ")"; // incoming
-			System.out.println(sql_query);
 
 			result = statement.executeQuery(sql_query);
 			while (result.next()) {
@@ -58,7 +63,6 @@ public class UserInfoHack {
 			System.out.println("Written incoming messages for " + table);
 
 			sql_query = "SELECT from_id, message FROM " + table + " where from_id in  (" + usersToGet + ")"; // outgoing
-			System.out.println(sql_query);
 
 			result = statement.executeQuery(sql_query);
 			while (result.next()) {
@@ -74,18 +78,19 @@ public class UserInfoHack {
 		statement.close();
 		incomingWriter.close();
 		outgoingWriter.close();
-		System.out.println("Messages written");
+		System.out.println("Messages written to files");
 
 		processMessages();
 	}
 
+	// find users who used top n words in messages
 	public static void processMessages() throws Exception{
 		BufferedReader br = new BufferedReader(new FileReader(OUTGOING_MESSAGES_FILE));
 		String message;
 		String word;
 		Long uid = 0L;
 		boolean first = true;
-		ArrayList<String> topNWords = PredictiveWords.getTopN(5);
+		ArrayList<String> topNWords = PredictiveWords.getTopN(TOPN);
 
 		outer:
 			while ((message = br.readLine()) != null){			
@@ -96,9 +101,7 @@ public class UserInfoHack {
 					if (first){
 						try {
 							uid = Long.parseLong(word);
-						} catch (Exception e){
-							uid = uid;
-						}
+						} catch (Exception e){ }
 						if (OUTGOING_SEEN.contains(uid))
 							continue outer;
 						first = false;
@@ -126,9 +129,7 @@ public class UserInfoHack {
 					if (first){
 						try {
 							uid = Long.parseLong(word);
-						} catch (Exception e){
-							uid = uid;
-						}
+						} catch (Exception e){ }
 						if (INCOMING_SEEN.contains(uid))
 							continue outer;
 						first = false;
@@ -149,6 +150,7 @@ public class UserInfoHack {
 		writeMessages();
 	}
 
+	// write users to files
 	public static void writeMessages() throws Exception{
 		PrintWriter writer = new PrintWriter(OUTGOING);
 		for (Long uid : OUTGOING_SEEN)
@@ -160,6 +162,32 @@ public class UserInfoHack {
 			writer.println(uid);
 		writer.close();
 	}
+	
+	// users who have said a top n word in an outgiong message
+	public static Set<Long> getSeenOutgoing() throws Exception{
+		HashSet<Long> users = new HashSet<Long>();
+		String message;
+		
+		BufferedReader br = new BufferedReader(new FileReader(OUTGOING));
+		while ((message = br.readLine()) != null){
+			users.add(Long.parseLong(message));
+		}
+		
+		return users;
+	}
+	
+	// users who have said a top n word in an incoming message
+	public static Set<Long> getSeenIncoming() throws Exception{
+		HashSet<Long> users = new HashSet<Long>();
+		String message;
+		
+		BufferedReader br = new BufferedReader(new FileReader(INCOMING));
+		while ((message = br.readLine()) != null){
+			users.add(Long.parseLong(message));
+		}
+		
+		return users;
+	}	
 	
 	public static void main(String[] args) throws Exception{
 		DataGeneratorPassiveActive.populateCachedData(true);
