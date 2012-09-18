4b65d390880a059c05839d1f1315a34c636d903c
rileyjkidd@gmail.com <rileyjkidd@gmail.com@63c62c83-405f-8d15-9062-c0fcbd917c60>
2012-09-18 02:17:21 +0000

    Set up for pages

--- trunk/social-recommendation/src/project/riley/predictor/Launcher.java
+++ trunk/social-recommendation/src/project/riley/predictor/Launcher.java
@@ -138,7 +138,7 @@ public class Launcher {
 				if (p.getName().contains("NaiveBayes") || p.getName().contains("LogisticRegression") || p.getName().contains("SVMLibSVM") || p.getName().contains("SVMLibLinear")){
 					System.out.println("Running predictors on " + DATA_FILE + " using " +  name +  " size " + i);
 					writer.println("Running predictors on " + DATA_FILE + " using " +  name +  " size " + i);
-					p.runTests(DATA_FILE /* file to use */, NUM_FOLDS /* folds to use */, 0 /* test threshold */, 0 /*groups size*/, 0/*pages size*/, i/*messages size*/, writer /* file to write */, false, false, false, false, true);
+					p.runTests(DATA_FILE /* file to use */, NUM_FOLDS /* folds to use */, 0 /* test threshold */, 0 /*groups size*/, i/*pages size*/, 0/*messages size*/, writer /* file to write */, false, false, true, false, false);
 				}
 			}
 	}
@@ -150,23 +150,15 @@ public class Launcher {
 
 		Date dNow = new Date();
 		SimpleDateFormat ft = new SimpleDateFormat ("dd_MM_yyyy");
-		String outName = "traits_results_" + ft.format(dNow) + ".txt"; 
+		String outName = "pages_results_" + ft.format(dNow) + ".txt"; 
 
 		writer = new PrintWriter(outName);		
 
 		//launcher.launchThresholds();
 		//launcher.launchSizeComparisons("group");		
-		//launcher.launchSizeComparisons("pages");
+		launcher.launchSizeComparisons("pages");
 		//launcher.launchSizeComparisons("messages");
-		//launcher.launchFlags();
-		
-		for (Predictor p : predictors){
-			if (p.getName().contains("NaiveBayes") || p.getName().contains("LogisticRegression") || p.getName().contains("SVMLibSVM") || p.getName().contains("SVMLibLinear")){
-				System.out.println("Running predictors on " + DATA_FILE + " using traits");
-				writer.println("Running predictors on " + DATA_FILE + " using traits");
-				p.runTests(DATA_FILE /* file to use */, NUM_FOLDS /* folds to use */, 0 /* test threshold */, 0 /*groups size*/, 0/*pages size*/, 0/*messages size*/, writer /* file to write */, false, false, false, true, false);
-			}
-		}
+		//launcher.launchFlags();				
 		
 		System.out.println("Finished writing to file " + outName);
 		writer.close();
