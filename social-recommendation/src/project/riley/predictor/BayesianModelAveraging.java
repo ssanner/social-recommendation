7f2c0c43b57c1ede465a71d741739f985e7bbf68
rileyjkidd <rileyjkidd@gmail.com>
2012-10-07 13:20:00 +0000

    bma updates

--- social-recommendation/src/project/riley/predictor/BayesianModelAveraging.java
+++ social-recommendation/src/project/riley/predictor/BayesianModelAveraging.java
@@ -52,61 +52,51 @@ public class BayesianModelAveraging extends Predictor {
 		data.setInteractions(((flag == 1 || flag == 8) ? true : false));
 		data.setDemographics(((flag == 2 || flag == 8) ? true : false));
 		data.setTraits(((flag == 3 || flag == 8) ? true : false));
-		data.setGroups(((flag == 4 || flag == 8) ? true : false), Launcher.NB_GROUPS_SIZE_OVERRIDE);
-		data.setPages(((flag == 5 || flag == 8) ? true : false), Launcher.NB_PAGES_SIZE_OVERRIDE);
-		data.setOutgoingMessages(((flag == 6 || flag == 8) ? true : false), Launcher.NB_OUTGOING_SIZE_OVERRIDE);
-		data.setIncomingMessages(((flag == 7 || flag == 8) ? true : false), Launcher.NB_INCOMING_SIZE_OVERRIDE);
+		data.setGroups(((flag == 4 || flag == 8) ? true : false), Launcher.LR_GROUPS_SIZE_OVERRIDE);
+		data.setPages(((flag == 5 || flag == 8) ? true : false), Launcher.LR_PAGES_SIZE_OVERRIDE);
+		data.setOutgoingMessages(((flag == 6 || flag == 8) ? true : false), Launcher.LR_OUTGOING_SIZE_OVERRIDE);
+		data.setIncomingMessages(((flag == 7 || flag == 8) ? true : false), Launcher.LR_INCOMING_SIZE_OVERRIDE);
 		data.setFileName(name);
 
 		return data;
-	}
-
-	// @author Scott Sanner (ssanner@gmail.com)
-	// for a,b given log versions log_a, log_b returns log(a + b)
-	public static double LogSum(double log_a, double log_b) {
-		double max = Math.max(log_a, log_b);
-		double sum = Math.exp(log_a - max) + Math.exp(log_b - max);
-		return Math.log(sum) + max;
-	}
-
-	public static double sigmoid(double t){
-		double k = 1.0;
-		return 1.0 / (1.0 + Math.pow(Math.E,(k * -t)));
-	}		
+	}	
 
 	@Override
 	public void train() {
 		predictors = setUp();		
 		weights = new double[predictors.length][Launcher.NUM_FOLDS][2];		// weights for each predictor of wm_0 & wm_1 at each fold
-			
+
 		for (int i = 0; i < predictors.length; i++){						// initialise weights
 			for (int j = 0; j < Launcher.NUM_FOLDS; j++){
 				weights[i][j][0] = 1.0 / predictors.length;
 				weights[i][j][1] = 1.0 / predictors.length;
 			}
 		}
-		
+
 		probabilities = new HashMap[predictors.length][Launcher.NUM_FOLDS];
 		double[][] normals = new double[Launcher.NUM_FOLDS][2];				// normals
 
 		for (int i = 0; i < predictors.length; i++){			
 
 			System.out.println("BMA training " + predictors[i].getName());
-			
+
 			for (int j = 0; j < Launcher.NUM_FOLDS; j++){
-				
+
 				System.out.println("Fold " + j);
-				
+
 				String trainName = Launcher.DATA_FILE + ".train." + (j+1);
 				String testName  = Launcher.DATA_FILE + ".test."  + (j+1);
 
 				predictors[i]._trainData = getArff(i,0,trainName);
 				predictors[i]._testData = getArff(i,0,testName);
-				
+
 				System.out.println(predictors[i]._trainData.getFlags());
 
-				if (predictors[i] instanceof LogisticRegression){
-					predictors[i].train();										
+				if (predictors[i] instanceof LogisticRegression){			// populate probabilities for SMB or LR 
+					predictors[i].train();	
+					for (DataEntry de : predictors[i]._testData._data) {
+						predictors[i].evaluate(de);								 
+					}	
 				} else {
 					LinkRecommenderArff.setType(Constants.SOCIAL);		
 					try {
@@ -116,13 +106,9 @@ public class BayesianModelAveraging extends Predictor {
 					}
 				}
 
-				for (DataEntry de : predictors[i]._testData._data) {
-					predictors[i].evaluate(de);								// populate probabilities 
-				}	
-
 				probabilities[i][j] = predictors[i].getProbabilities(); 	// uid -> (link_id,probability)
 
-				for (DataEntry de : predictors[i]._testData._data) {		// calculate w_m
+				for (DataEntry de : predictors[i]._testData._data) {		// calculate weights
 					Long uid = ((Double)de.getData(0)).longValue();
 					Long link_id = ((Double)de.getData(1)).longValue();
 					int friend_liked = de.friendLiked;
@@ -143,19 +129,14 @@ public class BayesianModelAveraging extends Predictor {
 			}
 		}		
 
-		for (int i = 0; i < predictors.length; i++){				// normalise
+		for (int i = 0; i < predictors.length; i++){				// normalise weights
 			for (int j = 0; j < Launcher.NUM_FOLDS; j++){
 				weights[i][j][0] /= normals[j][0];
 				weights[i][j][1] /= normals[j][1];
-				System.out.println(i + " " + j + " " + weights[i][j][0] + " " + weights[i][j][1]);
+				//System.out.println(i + " " + j + " " + weights[i][j][0] + " " + weights[i][j][1]);
 			}
 		}
 
-	}
-
-	public static void main(String[] args) {
-		BayesianModelAveraging bma = new BayesianModelAveraging();
-		bma.train();
 	}
 
 	@Override
@@ -175,6 +156,19 @@ public class BayesianModelAveraging extends Predictor {
 		return prob > 0.5 ? 0 : 1;
 	}
 
+	// @author Scott Sanner (ssanner@gmail.com)
+	// for a,b given log versions log_a, log_b returns log(a + b)
+	public static double LogSum(double log_a, double log_b) {
+		double max = Math.max(log_a, log_b);
+		double sum = Math.exp(log_a - max) + Math.exp(log_b - max);
+		return Math.log(sum) + max;
+	}
+
+	public static double sigmoid(double t){
+		double k = 1.0;
+		return 1.0 / (1.0 + Math.pow(Math.E,(k * -t)));
+	}		
+
 	@Override
 	public void clear() {
 		predictors = null;
@@ -190,6 +184,11 @@ public class BayesianModelAveraging extends Predictor {
 	@Override
 	public String getName() {
 		return "Bayesian Model Averaging";
+	}
+
+	public static void main(String[] args) {
+		BayesianModelAveraging bma = new BayesianModelAveraging();
+		bma.train();
 	}
 
 }
